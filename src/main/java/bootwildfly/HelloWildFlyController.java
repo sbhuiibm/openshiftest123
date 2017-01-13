472e22bd93c6fc0a79c887ed917f17b892a4277f
sbhuiibm <sbhui.ibm@gmail.com>
2017-01-13 16:08:06 +0800

    2nd commit

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,14 +1,24 @@
 package bootwildfly;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.demo.common.GreetingsResponse;
+
 @RestController
+@RequestMapping("/benTest")
 public class HelloWildFlyController {
 
 
-    @RequestMapping("hello")
-    public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly");
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value="/greetings", method= RequestMethod.GET)	    
+    @ResponseBody
+    public  GreetingsResponse greetings() {
+    	GreetingsResponse greetingsResponse=new GreetingsResponse();
+    	greetingsResponse.setGreetings("Hello, world!");
+        return greetingsResponse;
     }
 }
---
+++ src/main/java/com/demo/common/GreetingsResponse.java
@@ -0,0 +1,15 @@
+package com.demo.common;
+
+public class GreetingsResponse {
+
+	String greetings="";
+
+	public String getGreetings() {
+		return greetings;
+	}
+
+	public void setGreetings(String greetings) {
+		this.greetings = greetings;
+	}
+	
+}
